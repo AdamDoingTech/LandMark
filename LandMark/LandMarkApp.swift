fb1fbc21790f02c51fc8be53266a6cecbb1ed061
AtomLabX <olmpy@hotmail.com>
2022-06-10 00:35:27 -0600

    Initial Commit

---
+++ LandMark/LandMarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandMarkApp.swift
+//  LandMark
+//
+//  Created by Adam on 6/10/22.
+//
+
+import SwiftUI
+
+@main
+struct LandMarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
